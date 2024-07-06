0d4cf830927505ddd1c8c8d6bd1b5d8491156084
rd8834172 <98300291+rd8834172@users.noreply.github.com>
2024-07-06 06:49:33 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.innerText = '';
+}
+
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1);
+}
+
+function appendCharacter(char) {
+    display.innerText += char;
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (e) {
+        display.innerText = 'Error';
+    }
+}
